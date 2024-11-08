35f6eeadc3b0bca4874de8c33e645bd54abaf5b5
Anaconda123324 <alina.nozdrina.98@gmail.com>
2024-11-08 16:49:26 +0000

    Добавила классную работу

---
+++ classwork/2.py
@@ -0,0 +1,5 @@
+import calendar
+
+user_input=int(input('Введите номер месяца: '))
+
+print(calendar.month_name[user_input])
---
+++ classwork/4.py
@@ -0,0 +1,17 @@
+'''
+def numbers(*numb):
+    if numb == :
+        pass
+    else:
+        return sorted(a, b)
+'''
+    
+user_input=list(map(float, (input('Введите два числа через пробел: ')).split()))
+
+if user_input[0] == user_input[1] :
+    pass
+else:
+    print(sorted(user_input))
+
+
+#print(numbers(user_input[0], user_input[1]))

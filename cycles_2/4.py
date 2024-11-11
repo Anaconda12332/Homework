83b6d4198d782b14864465f9e633a94998bd8b0f
Anaconda123324 <alina.nozdrina.98@gmail.com>
2024-11-11 15:56:57 +0000

    +

--- cycles_2/4.py
+++ cycles_2/4.py
@@ -1,10 +1,10 @@
-def a(num):
+def count(num):
     return f'Сумма чисел:  {sum(all)}', f'Максимум чисел: {max(all)}', f'Минимум чисел: {min(all)}'
 
 all=[]
 while True:
     user_input=float(input('Введите число: '))
     all.append(user_input)
-    print( *a(user_input), sep='\n'  )  if user_input != 7 else print('Good bye!')
+    print( *count(user_input), sep='\n'  )  if user_input != 7 else print('Good bye!')
     if user_input == 7.0:
         break

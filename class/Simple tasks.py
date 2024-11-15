e60aeebf1342817a727ae95b6254037d3ffca0ac
Anaconda123324 <alina.nozdrina.98@gmail.com>
2024-11-15 18:37:24 +0000

    classwork

--- class/Simple tasks.py
+++ class/Simple tasks.py
@@ -11,24 +11,50 @@ print(m /  100)
 #3
 
 m=float(input('Введите кг: '))
-print(m /  100)
-
-#4
-
-m=float(input('Введите кг: '))
 print(m /  1000)
 
-#5
+#4
 
 m=float(input('Введите метры: '))
 print(m /  1000)
 
 
-#6
+#5
 
 days=243
 print('С какого то момента прошло: {}'.format(days // 7))
 
+#6
+
+n= 111111
+chas= n / 3600
+miin= chas-round(chas)
+sek= (miin-round(miin)) / 60
+
+print(chas, miin, sek)
+
+#исправить
+
 #7
 
+print(543// 130)
 
+#8
+
+k=int(input(': '))
+
+if k % 7 == 0:
+    print('Понедельник')
+
+elif (k-1) % 7 == 0:
+     print('Вторник')
+elif (k-2) % 7 == 0:
+     print('Среда')
+elif (k-3) % 7 == 0:
+     print('Четверг')
+elif (k-4) % 7 == 0:
+     print('Пятница')
+elif (k-5) % 7 == 0:
+     print('Суббота')
+elif (k-6) % 7 == 0:
+     print('Воскресенье')
--- prost.py
+++ prost.py
@@ -1,6 +1,20 @@
-n= 111111
-chas= n / 3600
-miin= (chas-round(chas)) / 360
-sek= (miin-round(miin)) / 60
+#days={1: 'Понедельник',2: 'Вторник',3: 'Среда',4: 'Четверг',5: 'Пятница',6: 'Суббота',7: 'Воскресенье'}
 
-print(chas, miin, sek)
+us=int(input(':'))
+
+a=[i for i in range(1, 366, 7)]
+
+if us in a:
+    print('понедельник')
+elif us -1 in a:
+    print('вт')
+elif us -2 in a:
+    print('ср')
+elif us -3 in a:
+    print('чт')
+elif us -4 in a:
+    print('пт')
+elif us -5 in a:
+    print('сб')
+elif us -6 in a:
+    print('вс')

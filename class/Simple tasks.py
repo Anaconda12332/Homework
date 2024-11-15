79908adb38a73ed62782705ea88a1ae6695103e0
Anaconda123324 <alina.nozdrina.98@gmail.com>
2024-11-15 17:28:16 +0000

    +

---
+++ class/Simple tasks.py
@@ -0,0 +1,34 @@
+#1
+
+sm=float(input('Введите сантиметры: '))
+print(sm /  100)
+
+#2
+
+m=float(input('Введите кг: '))
+print(m /  100)
+
+#3
+
+m=float(input('Введите кг: '))
+print(m /  100)
+
+#4
+
+m=float(input('Введите кг: '))
+print(m /  1000)
+
+#5
+
+m=float(input('Введите метры: '))
+print(m /  1000)
+
+
+#6
+
+days=243
+print('С какого то момента прошло: {}'.format(days // 7))
+
+#7
+
+
--- prost.py
+++ prost.py
@@ -1,13 +1,6 @@
+n= 111111
+chas= n / 3600
+miin= (chas-round(chas)) / 360
+sek= (miin-round(miin)) / 60
 
-user_input=int(input('Введите первое число: '))
-user_input_2=int(input('Введите второе число: '))
-
-a=[i for i in range(user_input, user_input_2 + 1) if i %2 == 0]
-b=[i for i in range(user_input, user_input_2 + 1) if i %2 != 0]
-d=[i for i in range(user_input, user_input_2 + 1) if i %9 == 0]
-
-
-print(f'Сумма четных чисел: {sum(a)}\nСумма нечетных чисел: {sum(b)}\nСумма чисел, кратных 9: {sum(d)}')
-print(f'Среднее арифметическое четных чисел: ', sum(a) / len(a))
-print(f'Среднее арифметическое нечетных чисел: ', sum(b) / len(b))
-print(f'Среднее арифметическое чисел, кратных 9: ', sum(d) / len(d))
+print(chas, miin, sek)

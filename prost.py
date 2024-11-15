14fe221d8be8e5de2b584036c576ac16f8ac308f
Anaconda123324 <alina.nozdrina.98@gmail.com>
2024-11-15 18:45:48 +0000

    +

--- prost.py
+++ prost.py
@@ -1,20 +1,20 @@
-#days={1: 'Понедельник',2: 'Вторник',3: 'Среда',4: 'Четверг',5: 'Пятница',6: 'Суббота',7: 'Воскресенье'}
+days={1: 'Понедельник',2: 'Вторник',3: 'Среда',4: 'Четверг',5: 'Пятница',6: 'Суббота',7: 'Воскресенье'}
 
 us=int(input(':'))
 
 a=[i for i in range(1, 366, 7)]
 
 if us in a:
-    print('понедельник')
+    print(days[1])
 elif us -1 in a:
-    print('вт')
+    print(days[2])
 elif us -2 in a:
-    print('ср')
+    print(days[3])
 elif us -3 in a:
-    print('чт')
+    print(days[4])
 elif us -4 in a:
-    print('пт')
+    print(days[5])
 elif us -5 in a:
-    print('сб')
+    print(days[6])
 elif us -6 in a:
-    print('вс')
+    print(days[7])

7b5d74f754a5361f51d28e8bc506aab86d922b0b
Alina <alina@PC.com>
2025-06-18 22:12:45 +0300

    class_work_online_9 доделала

--- Python/class_work_online_9/bridge.py
+++ Python/class_work_online_9/bridge.py
@@ -10,8 +10,83 @@ MultipleChoiceQuestion несколько правильных ответов
 AnswerChecker def check()
 
 реализации
-StrictChecker точное совпадение
+AnswerChecker точное совпадение
 PartialChecker частичное совпадение
 
 Question содержит ссылку на AnswerChecker
 """
+from abc import ABC, abstractmethod
+
+
+class AnswerChecker(ABC):
+    @abstractmethod
+    def check(self, correct_answer, user_answer):
+        pass
+
+
+class StrictChecker(AnswerChecker):
+    def check(self, correct_answer, user_answer):
+        return correct_answer == user_answer
+
+
+class PartialChecker(AnswerChecker):
+    def check(self, correct_answer, user_answer):
+        if not isinstance(correct_answer, set) or not isinstance(user_answer, set):
+            return False
+        check = correct_answer & user_answer
+        return len(check) / len(user_answer) >= 0.5
+
+
+class Question(ABC):
+    def __init__(self,  text, correct_answer, checker: AnswerChecker):
+        self.text = text
+        self.correct_answer = correct_answer
+        self.checker = checker
+
+    @abstractmethod
+    def check_answer(self, user_answer):
+        pass
+
+
+class SingleChoiceQuestion(Question):
+    def check_answer(self, user_answer):
+        return self.checker.check(self.correct_answer, user_answer)
+
+
+class MultipleChoiceQuestion(Question):
+    def check_answer(self, user_answer):
+        return self.checker.check(self.correct_answer, user_answer)
+
+
+q1 = SingleChoiceQuestion(
+    'Сборник географических карт',
+    'Атлас',
+    checker=StrictChecker()
+)
+
+q2 = MultipleChoiceQuestion(
+    'Назовите числа, которые больше 10, но меньше 15?',
+    correct_answer={11, 12, 13, 14},
+    checker=PartialChecker()
+)
+
+q3 = MultipleChoiceQuestion(
+    'Назовите числа, которые больше 10, но меньше 15?',
+    correct_answer={11, 12, 13, 14},
+    checker=StrictChecker()
+)
+
+print('*' * 30)
+print(q1.check_answer('Атлас'))
+print(q1.check_answer('Атлас1'))
+print(q1.check_answer(''))
+
+print('*' * 30)
+print(q2.check_answer({11, 12, 13, 14}))
+print(q2.check_answer({11, 12, 13, 14, 16}))
+print(q2.check_answer({}))
+
+print('*' * 30)
+print(q3.check_answer({11, 12, 13, 14}))
+print(q3.check_answer({11, 12, 13, 14, 16}))
+print(q3.check_answer({}))
--- Python/class_work_online_9/composite.py
+++ Python/class_work_online_9/composite.py
@@ -10,10 +10,106 @@ class Expression def evaluate
 классы-композиты
 Addition, Subtraction, Multiplication, Division принимают два дочерних
 выражения, которые реализуют интерфейс Expression
-
+        *
+       / \
+     +     -
+     / \  / \
+    2   3 4   1
 """
+from abc import ABC, abstractmethod
+
 # expr = Multiplication(
 #     Addition(Number(2), Number(3)),
 #     Subtraction(Number(4), Number(1))
 # )
 # print(expr.evaluate()) #15
+
+""""""
+
+
+class Expression(ABC):
+    @abstractmethod
+    def evaluate(self):
+        pass
+
+    @abstractmethod
+    def __str__(self):
+        pass
+
+
+class Number(Exception):
+    def __init__(self, number):
+        self.number = number
+
+    def evaluate(self):
+        return self.number
+
+    def __str__(self):
+        return str(self.number)
+
+
+class Addition(Expression):
+    def __init__(self, left: Exception, right: Exception):
+        self.left = left
+        self.right = right
+
+    def evaluate(self):
+        return self.left.evaluate() + self.right.evaluate()
+
+    def __str__(self):
+        return f'({self.left} + {self.right})'
+
+
+class Subtraction(Expression):
+    def __init__(self, left: Exception, right: Exception):
+        self.left = left
+        self.right = right
+
+    def evaluate(self):
+        return self.left.evaluate() - self.right.evaluate()
+
+    def __str__(self):
+        return f'({self.left} - {self.right})'
+
+
+class Multiplication(Expression):
+    def __init__(self, left: Exception, right: Exception):
+        self.left = left
+        self.right = right
+
+    def evaluate(self):
+        return self.left.evaluate() * self.right.evaluate()
+
+    def __str__(self):
+        return f'({self.left} * {self.right})'
+
+
+class Division(Expression):
+    def __init__(self, left: Exception, right: Exception):
+        self.left = left
+        self.right = right
+
+    def evaluate(self):
+        return self.left.evaluate() / self.right.evaluate()
+
+    def __str__(self):
+        return f'({self.left} / {self.right})'
+
+
+expr = Multiplication(
+    Addition(Number(2), Number(3)),
+    Subtraction(Number(4), Number(1))
+)
+
+expr2 = Division(
+                Multiplication(
+                                Addition(Number(2), Number(3)),
+                                Subtraction(Number(4), Number(1)),
+                ),
+                Multiplication(
+                                Addition(Number(2), Number(3)),
+                                Subtraction(Number(4), Number(3)),
+                ))
+
+print(expr, '=', expr.evaluate())
+print(expr2, '=', expr2.evaluate())
